91dff203a832f879db8f756927c3a29bdbb9cd37
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2021-09-23 23:26:23 -0300

    add helpers HTTP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import CrudApp  from './Components/CrudApp';
+import CrudApp from "./Components/CrudApp";
+import CrudApi from "./Components/CrudApi";
 
 function App() {
   return (
     <div className="App">
+      <hr />
+      <CrudApi />
+      <hr />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <hr />
       <CrudApp />
+      <hr />
     </div>
   );
 }
